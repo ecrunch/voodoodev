496e3cee6672af35870692f69a142e838d7571b2
zack <zackbotkin@gmail.com>
2015-03-31 21:49:47 -0500

    Fix logout bug

--- public/javascripts/AngularApp.js
+++ public/javascripts/AngularApp.js
@@ -31,12 +31,15 @@ function($scope, $state, auth){
 }]);
 
 app.controller('NavCtrl',
-['$scope', 'auth',
-function($scope, auth){
+['$scope', 'auth','$state',
+function($scope, auth, $state ){
 	
 	$scope.isLoggedIn = auth.isLoggedIn;
 	$scope.currentUser = auth.currentUser;
-	$scope.logOut = auth.logOut;
+	$scope.logOut = function(){ 
+		auth.logOut();
+		$state.go('login');
+	};
 
 }]);
  
