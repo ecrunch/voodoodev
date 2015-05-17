62f46c1379c268066bd813535cbb27d10cd23bd0
zack <zackbotkin@gmail.com>
2015-05-17 14:35:39 -0500

    updated scheduler to be clearer and worked with functions to finalize usablity

--- public/javascripts/controllers/ScheduleCtrl.js
+++ public/javascripts/controllers/ScheduleCtrl.js
@@ -5,30 +5,35 @@ app.controller('ScheduleCtrl', [
 '$scope', '$stateParams', 'Schedule', '$interval',
 function($scope, $stateParams, Schedule, $interval) {
         var j;
-        var i=0;
-        $scope.tk=0;
+        var i = 0;
+	//timerStatus handles what buttons should be showing on the userside
+        $scope.timerStatus = 0;
 
         $scope.newSchedule = function() {
                 Schedule.purge();
                 Schedule.createNew($scope.userTime).then(function(){
                         $scope.items = Schedule.items;
                         j=undefined;
-                        $scope.iTime = 0;
+                        $scope.timeLeft = 0;
+			$scope.timerStatus = 1;
                 });
         };
-        $scope.storeTime = function(id,trackt){
+        $scope.storeTime = function(){
                 id =  $scope.id;
-                trackt = (($scope.tTime-$scope.iTime)/60000);
-                Schedule.storeTime(id, trackt);
+                trackedTime = (($scope.totalTime-$scope.timeLeft)/60000);
+                Schedule.storeTime(id, trackedTime);
 
         };
 
 
         $scope.scheduleTimer = function(){
-        if ( angular.isDefined(stop) ) return;
-        if (angular.isUndefined(j)){
+        //prevents it going to the next item if it is already going and not done
+	if ( angular.isDefined(stop) ) return;
+        //starts at first item of array if the schedule was just started 
+	if (angular.isUndefined(j)){
                 j=i;
                 $scope.pass();
+	//if the schedule has been start adds to go to next item in the schedule
         } else{
                 j=j+1;
                 $scope.pass();
@@ -37,10 +42,10 @@ function($scope, $stateParams, Schedule, $interval) {
 
         $scope.pass =function(){
                 var item = $scope.items[j];
-                var iTime = (item.minutes)*60000;
-                $scope.iTime = iTime;
-                $scope.tTime = iTime;
-                $scope.tk=1;
+                var timeLeft = (item.minutes)*60000;
+                $scope.timeLeft = timeLeft;
+                $scope.totalTime = timeLeft;
+                $scope.timerStatus=2;
                 $scope.display = item.details.description;
                 $scope.id = item.details.id;
                 $scope.timer();
@@ -49,12 +54,13 @@ function($scope, $stateParams, Schedule, $interval) {
         var stop;
         $scope.timer = function(){
         stop = $interval(function() {
-                if ($scope.iTime > 0) {
-                        $scope.timerTimes = moment.duration($scope.iTime).seconds();
-                        $scope.timerTimem = moment.duration($scope.iTime).minutes();
-                        $scope.iTime = $scope.iTime - 1000;
+                if ($scope.timeLeft > 0) {
+                        $scope.timerTimes = moment.duration($scope.timeLeft).seconds();
+                        $scope.timerTimem = moment.duration($scope.timeLeft).minutes();
+                        $scope.timeLeft = $scope.timeLeft - 1000;
                 } else {
-                        $scope.tk=0;
+			$scope.storeTime();
+                        $scope.timerStatus = 1;
                         $scope.stopTimer();
                 }
         }, 1000);
@@ -63,20 +69,24 @@ function($scope, $stateParams, Schedule, $interval) {
 
         $scope.stopTimer = function() {
                 if (angular.isDefined(stop)) {
-                        $interval.cancel(stop);
+                        $scope.timerStatus = 3;
+			$interval.cancel(stop);
                         stop = undefined;
                 }
         };
 
         $scope.startTime=function(){
-                $scope.timer();
+                $scope.timerStatus = 2;
+		$scope.timer();
         };
 
         $scope.skipTimer = function(){
                 $scope.stopTimer();
-                $scope.display='';
-                $scope.iTime ='';
+                $scope.storeTime();
+		$scope.display='';
+                $scope.timeLeft ='';
                 $scope.scheduleTimer();
+		
         };
 
         $scope.$on('$destroy', function() {
@@ -84,6 +94,5 @@ function($scope, $stateParams, Schedule, $interval) {
           $scope.stopTimer();
         });
 
-        $scope.newSchedule();
 
 }]);
