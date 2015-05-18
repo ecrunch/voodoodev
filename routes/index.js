fd38858615fe7a8a09e1a92e50c0ef557b64e4dc
zack <zackbotkin@gmail.com>
2015-05-17 21:14:45 -0500

    removed the huge callback from index.js routes

--- routes/index.js
+++ routes/index.js
@@ -136,6 +136,8 @@ module.exports = function(router, User, passport, Breather, Course, Task, Commen
 
 
 	});
+
+
 	router.post('/user/courses', auth, function(req, res, next) {
 
 		var userId = req.payload.id;
@@ -176,83 +178,6 @@ module.exports = function(router, User, passport, Breather, Course, Task, Commen
 
 	});
 
-
-	router.post('/user', auth, function(req, res, next) {
-
-		var userId = req.payload.id;
-		User.findById(userId, function(err, user) {
-			if(err) {
-			// do whatever
-				console.log(err);
-			}
-			else {
-				var toRet = {};
-
-
-				// the fuck is this code doing ?
-
-				toRet.breatherIds = user.myBreathers.filter(function(item, pos){
-					return user.myBreathers.indexOf(item) == pos;
-				});
-
-				toRet.courseIds = user.myCourses.filter(function(item, pos){
-                                	return user.myCourses.indexOf(item) == pos;
-                        	});
-
-				toRet.taskIds = user.myTasks.filter(function(item, pos){
-                                	return user.myTasks.indexOf(item) == pos;
-                        	});
-
-				Breather.find({
-					'_id': { 
-						$in: toRet.breatherIds
-					}
-				}, 
-				function(err, breathers) {
-	
-					if(err) {
-						console.log(err);
-						console.log("Cannot get breathers");
-					}
-					else {
-						toRet.breathers = breathers;
-
-						Course.find({
-                                			'_id': {
-                                        			$in: toRet.courseIds
-                                			}
-                        			},
-                        			function(err, courses) {
-                                			if(err) {
-                                        			console.log(err);
-                                        			console.log("Cannot get courses");
-                                			}
-                                			else {
-                                        			toRet.courses = courses;
-								Task.find({
-									'userId': userId
-                        					},
-                        					function(err, tasks) {
-									if(err) {
-                                        					console.log(err);
-                                						console.log("Cannot get tasks");
-									}
-                                					else {
-                                        					toRet.tasks = tasks;
-                                        					res.json(toRet);
-                                					}
-                        					});  //end of tasks callback
-                                			}  //end of else
-                        			});  //end of courses callback
-					} //end of else
-				}); //end of breathers callback
-		
-			}  //end of else
-		}); // end of users callback
-
-		// TODO ^^ jesus fuck refactor this
-
-	});
 
 	router.param('comment', function(req, res, next, id) {
 
