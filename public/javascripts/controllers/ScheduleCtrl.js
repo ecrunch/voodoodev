8379b0c72f4995ed93ae2d6f1358199e42c2f07f
zack <zackbotkin@gmail.com>
2015-05-20 00:35:33 -0500

    schedule items drop when they are skipped or completed

--- public/javascripts/controllers/ScheduleCtrl.js
+++ public/javascripts/controllers/ScheduleCtrl.js
@@ -17,9 +17,8 @@ function($scope, $stateParams, Schedule, $interval) {
 		}
 	}
 
-	// TODO fix these
-	var j;
-        var i = 0;
+	var currentIndex = 0;
+	
 	//timerStatus handles what buttons should be showing on the userside
         $scope.timerStatus = 0;
 
@@ -28,13 +27,23 @@ function($scope, $stateParams, Schedule, $interval) {
 
 		var numHours = validateTime($scope.userTime) ? $scope.userTime : defaultHours; // DEFAULT to 4 
 
+		// TODO : this is weird, we should just have it assemble the items
+		// and not have to build up an object
                 Schedule.createNew(numHours).then(function(){
                         $scope.items = Schedule.items;
-                        j=undefined;
+                        currentIndex = 0;
                         $scope.timeLeft = 0;
 			$scope.timerStatus = 1;
                 });
         };
+
+	
+	$scope.remoteItem = function(slot) {
+		console.log("Removing at slot: " + slot);
+		$scope.items.splice(slot, 1);
+	};
+
+
         $scope.storeTime = function(){
                 id =  $scope.id;
                 trackedTime = (($scope.totalTime-$scope.timeLeft)/60000);
@@ -44,43 +53,39 @@ function($scope, $stateParams, Schedule, $interval) {
 
 
         $scope.scheduleTimer = function(){
-        //prevents it going to the next item if it is already going and not done
-	if ( angular.isDefined(stop) ) return;
-        //starts at first item of array if the schedule was just started 
-	if (angular.isUndefined(j)){
-                j=i;
-                $scope.pass();
-	//if the schedule has been start adds to go to next item in the schedule
-        } else{
-                j=j+1;
-                $scope.pass();
-        }
+        	//prevents it going to the next item if it is already going and not done
+		if ( angular.isDefined(stop) ) return;        	
+		$scope.pass();
         };
 
-        $scope.pass =function(){
-                var item = $scope.items[j];
-                var timeLeft = (item.minutes)*60000;
-                $scope.timeLeft = timeLeft;
-                $scope.totalTime = timeLeft;
-                $scope.timerStatus=2;
-                $scope.display = item.details.description;
-                $scope.id = item.details.id;
-                $scope.timer();
-        };
+        $scope.pass = function(){
+                var item 		= $scope.items[currentIndex];
+		var timeLeft 		= (item.minutes)*60000;
+                $scope.timeLeft 	= timeLeft;
+                $scope.totalTime 	= timeLeft;
+                $scope.timerStatus	= 2;
+                $scope.display 		= item.details.description;
+                $scope.id 		= item.details.id;
+                $scope.timer(); 
+	};
+
 
         var stop;
+
         $scope.timer = function(){
-        stop = $interval(function() {
-                if ($scope.timeLeft > 0) {
-                        $scope.timerTimes = moment.duration($scope.timeLeft).seconds();
-                        $scope.timerTimem = moment.duration($scope.timeLeft).minutes();
-                        $scope.timeLeft = $scope.timeLeft - 1000;
-                } else {
-			$scope.storeTime();
-                        $scope.timerStatus = 1;
-                        $scope.stopTimer();
-                }
-        }, 1000);
+        	
+		stop = $interval(function() {
+                	if ($scope.timeLeft > 0) {
+                        	$scope.timerTimes = moment.duration($scope.timeLeft).seconds();
+                        	$scope.timerTimem = moment.duration($scope.timeLeft).minutes();
+                        	$scope.timeLeft = $scope.timeLeft - 1000;
+                	} else {
+				$scope.storeTime();
+				$scope.removeItem(currentIndex);
+                        	$scope.timerStatus = 1;
+                        	$scope.stopTimer();
+                	}
+        	}, 1000);
 
         };
 
@@ -92,7 +97,7 @@ function($scope, $stateParams, Schedule, $interval) {
                 }
         };
 
-        $scope.startTime=function(){
+        $scope.startTime = function(){
                 $scope.timerStatus = 2;
 		$scope.timer();
         };
@@ -103,6 +108,7 @@ function($scope, $stateParams, Schedule, $interval) {
 		$scope.display='';
                 $scope.timeLeft ='';
                 $scope.timerStatus = 2;
+		$scope.remoteItem(currentIndex);
 		$scope.scheduleTimer();
 		
         };
