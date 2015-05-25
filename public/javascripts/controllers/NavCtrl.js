d4541c8ed6c0618368c28f9997fb9051133cf8ba
zack <zackbotkin@gmail.com>
2015-05-25 17:28:49 -0500

    setting up nav bar look

--- public/javascripts/controllers/NavCtrl.js
+++ public/javascripts/controllers/NavCtrl.js
@@ -2,9 +2,10 @@ var app = angular.module('NavCtrl',[]);
 
 
 app.controller('NavCtrl',
-['$scope', 'auth','$state',
-function($scope, auth, $state ){
-
+['$scope', 'auth','$state', 'courses','Task',
+function($scope, auth, $state, courses, Task){
+	
+	
         $scope.isLoggedIn = auth.isLoggedIn;
         $scope.currentUser = auth.currentUser;
         $scope.logOut = function(){
@@ -13,6 +14,43 @@ function($scope, auth, $state ){
 		document.location.reload(true);
 
         };
+	
+	$scope.pendingAssignments = [];
+	
+	        $scope.getPendingAssignments = function() {
+                return courses.getPendingAssignments();
+        };
+        $scope.joined = function(assId){
+                courses.joined(assId);
+                };
+
+        $scope.joinAssignment = function(pending) {
+                console.log('called');
+		Task.create({
+                        description:    pending.name,
+                        dueDate:        pending.dueDate,
+                        type:           pending.type
+                });
+		location.reload();
+		$scope.Pcount =(Object.keys($scope.pendings).length);
+                $scope.joined(pending._id);
+        };
+
+
+        // only do this once the other shit is loaded? regardless, need to refactor
+        $scope.getPendingAssignments().then(
+                // success
+                function(data) {
+                        $scope.pendings = data.data;
+                        $scope.pendingAssignments = data;
+			console.log($scope.pendings); 
+               		$scope.Pcount =(Object.keys($scope.pendings).length);
+		},
+                // error
+                function() {
+                        console.log("Controller: Error loading pending assignments");
+                }
+        );
 
 }]);
 
