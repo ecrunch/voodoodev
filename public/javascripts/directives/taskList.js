61ffa005d8abac706e1c4cc7f5c4f966a088c9a2
zack <zackbotkin@gmail.com>
2015-06-22 13:41:41 -0500

    almost done with list directive, holds all schedule items and courses, gives the ability to add a task tho I am struggling on having it save to the db

--- public/javascripts/directives/taskList.js
+++ public/javascripts/directives/taskList.js
@@ -1,21 +1,42 @@
 var app = angular.module('taskListDirective',[]);
 
 app.controller('taskListCtrl', [
-'$scope', 'Task','auth',
-function($scope, Task, auth) {
+'$scope', 'Task','auth','breathers', 'courses',
+function($scope, Task, auth, breathers, courses) {
 	
 	$scope.user = auth.currentUser;
         $scope.isLoggedIn = auth.isLoggedIn;
         $scope.tasks = Task.tasks ;
+	$scope.breathers = breathers.breathers;
+	$scope.courses = courses.courses;
+	
+	$scope.addTask = function() {
+		console.log('Here');
+                if ($scope.newTaskDescription === '') {
+                        return;
+                }
+                Task.create({
+                        description: $scope.newTaskDescription,
+                        dueDate: $scope.newTaskDueDate,
+                        type:   $scope.newTaskType
+                });
+                $scope.newTaskDescription = '';
+                $scope.newTaskDueDate = '';
+                $scope.newTaskType = '';
+        };
 }]);	
 
 app.directive('taskList', function() {
 	return{
 		restrict: 'EA',
 		scope: {
-			tasks: '='
+			tasks: '=',
+			breathers: '=',
+			courses: '=',
+			statustype: '=',
+			addtask: '='
 		},
-		template:"<ul><li ng-repeat='task in tasks'><a>{{task.description}}</a></li></ul>",
+		templateUrl: 'temps/directive_partials/taskListDirective.html',
 		link : function(scope, element, attrs) {
 			console.log(scope.tasks);
 		}
